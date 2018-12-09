667f2f0310fc53f3ef1bc7dbe2ed7579bfcb8ffe
kml313 <kamal18kishore@gmail.com>
2018-12-09 12:50:34 +0100

    Relay example

--- script/ue4_relayExample.R
+++ script/ue4_relayExample.R
@@ -3,13 +3,14 @@ rm(list=ls())    #empty workspace if wanted ...
 
 ls()
 library(MASS)
-dimdoe_path<-"C:\\Dokumente und Einstellungen\\orth\\Desktop\\dim-doe\\"
-#dimdoe_path<-"W:\\projekte\\dim-doe\\"
-script_path<-paste(dimdoe_path,"R-skripte\\",sep="")
-script_path
-ex_path<-paste(dimdoe_path,"R-neue-Bspl\\",sep="")
-ex_path<-paste(ex_path,"UE4_RelayExample\\",sep="")
-ex_path
+
+#preparation
+dimdoe_path<-"../"
+script_path<-paste(dimdoe_path,"lib/",sep="")
+ex_path<-paste(dimdoe_path,"data/UE4_RelayExample/",sep="")
+
+#transferring functions
+#source(paste(script_path,"dim_doe_functions_dataList_Vin.R",sep=""))
 source(paste(script_path,"dim_doe_functions_TurnQual.R",sep=""))
 
 #problem specification
